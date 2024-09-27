f636f76411a0e460348bfae50b63d197817923e3
kvnbanunu <kvnbanunu@gmail.com>
2024-09-27 00:56:15 -0700

    Working on collecting arguments

--- src/client.c
+++ src/client.c
@@ -1,44 +1,102 @@
 /*
-* Kevin Nguyen
-* A00955925
-*/
+ * Kevin Nguyen
+ * A00955925
+ */
 
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 
-void print_usage();
+_Noreturn void print_usage(const char *prog_name);
 
 int main(int argc, char *argv[])
 {
-    int opt;
-    int messageSize;
-    const char* filter;
-    const char* message = NULL;
-
-    while ((opt = getopt(argc, argv, "f:"))!= -1)
+    int         opt;
+    int         argind      = 1;
+    size_t      messageSize = 0;
+    const char *filter      = NULL;
+    char       *message;
+    // Reading the option flags
+    while((opt = getopt(argc, argv, "f:")) != -1)
     {
-        switch(opt)
+        if(opt == 'f')
         {
-            case 'f':
-                if (sizeof(optarg) != 1)
-                {
-                    fprintf(stderr, "Error: Filter setting needs to be a single char\n");
-                    print_usage();
-                }
-                filter = optarg;
-                break;
-            default:
-                print_usage();
+            if(sizeof(optarg) != 1)
+            {
+                fprintf(stderr, "Error: Filter option needs to be a single char\n");
+                print_usage(argv[0]);
+            }
+            filter = optarg;
+            if(!(strcmp(filter, "U") == 0 || strcmp(filter, "L") == 0 || strcmp(filter, "N") == 0))
+            {
+                fprintf(stderr, "Error: Invalid filter option %s\n", optarg);
+                print_usage(argv[0]);
+            }
+        }
+        else
+        {
+            fprintf(stderr, "Invalid flag %d\n", opt);
+            print_usage(argv[0]);
         }
     }
+
+    // Checking if there are arguments after the filter option
+    if(optind == argc - 1)
+    {
+        fprintf(stderr, "Error: Message cannot be empty");
+        print_usage(argv[0]);
+    }
+
+    if(filter)
+    {
+        argind++;
+    }
+
+    if(argind == argc - 1)
+    {
+        fprintf(stderr, "Error: Message cannot be empty");
+        print_usage(argv[0]);
+    }
+
+    for(int i = argind; i < argc; i++)
+    {
+        messageSize += strlen(argv[i]) + 1;
+    }
+
+    if(messageSize > 0)
+    {
+        message = (char *)malloc(messageSize * sizeof(char));
+        strncpy(message, argv[argind], messageSize);
+    }
+    else
+    {
+        fprintf(stderr, "Error: Message cannot be empty");
+        print_usage(argv[0]);
+    }
+
+    // Add to message
+    for(int i = argind + 1; i < argc; i++)
+    {
+        strcat(message, argv[i]);
+        printf("%s\n", message);
+    }
+
+    // Testing the filter
+    if(filter == NULL)
+    {
+        printf("filter is null\n");
+    }
+    else
+    {
+        printf("%s", filter);
+    }
+    free(message);
     return EXIT_SUCCESS;
 }
 
-void print_usage()
+_Noreturn void print_usage(const char *prog_name)
 {
-    fprintf(stderr, "Usage: %s [-f filter (u|l|n)] [message]\n". argv[0]);
+    fprintf(stderr, "Usage: %s [-f filter] [message]\n\t-f\tU | uppercase\n\t\tL | lowercase\n\t\tN | null\n", prog_name);
     exit(EXIT_FAILURE);
 }
